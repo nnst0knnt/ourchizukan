bce76584743c21d80970eb0c1fb84d62f19bb73d
nnst0knnt <nnst0knnt@gmail.com>
2025-08-28 00:00:00 +0900

    ✨ add album routes

--- routes/endpoints/albums/index.ts
+++ routes/endpoints/albums/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 import { create } from "./create";
+import { get } from "./get";
+import { list } from "./list";
 
 /**
  * /api/albums
  *
  * アルバムに関するAPIをまとめたルートです。
  */
-export const albums = new Hono().post("/", ...create);
+export const albums = new Hono()
+  .get("/", ...list)
+  .post("/", ...create)
+  .get("/:id", ...get);
