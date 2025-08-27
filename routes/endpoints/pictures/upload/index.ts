f15ca69f1baac78103ee72b68c7c75baf7418c1f
nnst0knnt <nnst0knnt@gmail.com>
2025-08-28 00:00:00 +0900

    ✨ implement upload picture

--- routes/endpoints/pictures/upload/index.ts
+++ routes/endpoints/pictures/upload/index.ts
@@ -1,16 +1,83 @@
+import { eq } from "drizzle-orm";
 import { StatusCodes } from "http-status-codes";
+import { albums, pictures } from "@/database/schema";
+import { ObjectKey } from "@/models";
 import { validator } from "@/routes/middlewares";
+import { uuid } from "@/services/uuid";
 import { factory, toBody } from "../../../helpers";
 import { UploadPicture } from "./schema";
 
 export const upload = factory.createHandlers(
   validator.form(UploadPicture),
   async (context) => {
-    /** TODO */
     const body = (await toBody(context, UploadPicture))!;
 
-    console.log(body);
+    if (!body) {
+      return context.json(
+        { message: "何も送られていません" },
+        StatusCodes.BAD_REQUEST,
+      );
+    }
 
-    return context.json("写真をアップロードしました！", StatusCodes.OK);
+    if (body.albumId) {
+      const exists = !!(
+        await context.var.database
+          .select()
+          .from(albums)
+          .where(eq(albums.id, body.albumId))
+          .limit(1)
+      )[0];
+
+      if (!exists) {
+        return context.json(
+          { message: "そのアルバムは見覚えがないようです" },
+          StatusCodes.BAD_REQUEST,
+        );
+      }
+    }
+
+    const uploaded: (typeof pictures.$inferSelect)[] = [];
+    for (const file of body.files) {
+      const id = uuid();
+      const originalKey = `${ObjectKey.Picture.original}/${uuid()}`;
+      const thumbnailKey = `${ObjectKey.Picture.thumbnail}/${uuid()}`;
+      const now = Date.now();
+
+      const buffer = await file.arrayBuffer();
+
+      await context.var.buckets.pictures.put(originalKey, buffer, {
+        mime: file.type,
+      });
+
+      await context.var.buckets.pictures.put(thumbnailKey, buffer, {
+        mime: file.type,
+      });
+
+      await context.var.database.insert(pictures).values({
+        id,
+        albumId: body.albumId,
+        originalKey,
+        thumbnailKey,
+        takenAt: now,
+        createdAt: now,
+      });
+
+      uploaded.push({
+        id,
+        albumId: body.albumId,
+        originalKey,
+        thumbnailKey,
+        takenAt: now,
+        createdAt: now,
+      });
+    }
+
+    return context.json(
+      {
+        message: `${uploaded.length}枚の写真をアップロードしました`,
+        pictures: uploaded,
+      },
+      StatusCodes.CREATED,
+    );
   },
 );
