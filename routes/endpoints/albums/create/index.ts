4c8a4cd288dc41d16a4f8782fc18ca0be4e3a60f
nnst0knnt <nnst0knnt@gmail.com>
2025-08-28 00:00:00 +0900

    ✨ implement create album

--- routes/endpoints/albums/create/index.ts
+++ routes/endpoints/albums/create/index.ts
@@ -1,16 +1,30 @@
 import { StatusCodes } from "http-status-codes";
+import { albums } from "@/database/schema";
 import { validator } from "@/routes/middlewares";
+import { uuid } from "@/services/uuid";
 import { factory } from "../../../helpers";
 import { CreateAlbum } from "./schema";
 
 export const create = factory.createHandlers(
   validator.json(CreateAlbum),
   async (context) => {
-    /** TODO */
     const body = context.req.valid("json");
+    const id = uuid();
+    const now = Date.now();
 
-    console.log(body);
+    await context.var.database.insert(albums).values({
+      id,
+      title: body.title,
+      createdAt: now,
+    });
 
-    return context.json("アルバムを作成しました！", StatusCodes.OK);
+    return context.json(
+      {
+        id,
+        title: body.title,
+        createdAt: now,
+      },
+      StatusCodes.CREATED,
+    );
   },
 );
