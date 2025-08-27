24faff748960372759dc8746a13d13416bae2fa0
nnst0knnt <nnst0knnt@gmail.com>
2025-08-28 00:00:00 +0900

    ✨ ignore middleware in prefetch

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
+import type { MiddlewareConfigInput as Config } from "next/dist/build/segment-config/middleware/middleware-config";
 import { NextResponse } from "next/server";
 import { bind, factory } from "./routes/helpers";
 
-export const config = {
+export const config: Config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon*|icon.svg|manifest*|scripts).*)",
+    {
+      source:
+        "/((?!api|_next/static|_next/image|favicon*|icon.svg|manifest*|scripts).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
   ],
 };
 
