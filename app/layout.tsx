16a66ba9af8f03b56ee49731daf19ff08f4ed404
nnst0knnt <nnst0knnt@gmail.com>
2025-08-28 00:00:00 +0900

    💄 prevents zooming in when focusing

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
+  maximumScale: 1,
   userScalable: true,
   viewportFit: "cover",
 };
