5d33b94c94ea5f5bdd9c494bcc86f11fe69d292b
nnst0knnt <nnst0knnt@gmail.com>
2025-08-28 00:00:00 +0900

    ✨ automate sort import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@ import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { globalIgnores } from "@eslint/config-helpers";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import unicornPlugin from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +17,72 @@ const compat = new FlatCompat({
 const eslintConfig = [
   globalIgnores([".next", ".vercel", ".wrangler", "env.d.ts"]),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      unicorn: unicornPlugin,
+      import: importPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "object",
+            "type",
+            "index",
+          ],
+          "newlines-between": "always",
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: "react**",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "{next/**,next}",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "{@**}",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "{@/**}",
+              group: "internal",
+              position: "before",
+            },
+          ],
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
